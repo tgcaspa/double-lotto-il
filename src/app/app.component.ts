ddb18a3989312deccd8353dcb2a736747f39c7a7
tgcaspa <cmplerec@gmail.com>
2020-05-02 21:03:51 +0300

    Add A2HS configurations and assets.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -46,6 +46,40 @@ export class AppComponent implements OnInit {
       // } else {
       //   this.resultsSvc.pushPaisLastResult$(paisResult);
       // }
+
+      // Code to handle install prompt on desktop
+      this.initA2HSButton();
+    }
+
+    private initA2HSButton() {
+      let deferredPrompt;
+      const addBtn = document.querySelector<HTMLElement>('.btn-a2hs');
+      addBtn.style.display = 'none';
+
+      window.addEventListener('beforeinstallprompt', (e) => {
+        // Prevent Chrome 67 and earlier from automatically showing the prompt
+        e.preventDefault();
+        // Stash the event so it can be triggered later.
+        deferredPrompt = e;
+        // Update UI to notify the user they can add to home screen
+        addBtn.style.display = 'block';
+
+        addBtn.addEventListener('click', (e) => {
+          // hide our user interface that shows our A2HS button
+          addBtn.style.display = 'none';
+          // Show the prompt
+          deferredPrompt.prompt();
+          // Wait for the user to respond to the prompt
+          deferredPrompt.userChoice.then((choiceResult) => {
+            if (choiceResult.outcome === 'accepted') {
+              console.log('User accepted the A2HS prompt');
+            } else {
+              console.log('User dismissed the A2HS prompt');
+            }
+            deferredPrompt = null;
+          });
+        });
+      });
     }
 
 }
